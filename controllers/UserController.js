4215528b75ae5933bb3dc5dcd37e31e716c35623
unknown <iqbalraju123@gmail.com>
2023-01-20 23:06:10 +0600

    working on user controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,1 +1,21 @@
 const ErrorHandler = require('../utils/errorHandler');
+const catchAsyncErrors = require('../middleware/catchAsyncErrors');
+const User = require('../models/userModel');
+
+exports.registerUser = catchAsyncErrors(async(req,res,next)=>{
+    const {name, email, password} = req.body;
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        avatar:{
+            public_id: 'avatars/1',
+            url: 'https://res.cloudinary.com'
+        },
+    });
+
+    res.status(201).json({
+        success: true,
+    });
+})
